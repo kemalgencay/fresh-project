1dac9342adb4c327fe7ee1ef2d73bc35084e787a
kemalgencay <kemalgencay@gmail.com>
2024-01-04 22:09:28 +0300

    commit 07

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,8 +5,6 @@
 import * as $_name_ from "./routes/[name].tsx";
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
-import * as $api_joke from "./routes/api/joke.ts";
-import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $hakkimda from "./routes/hakkimda.tsx";
 import * as $index from "./routes/index.tsx";
 import * as $sozler from "./routes/sozler.tsx";
@@ -18,8 +16,6 @@ const manifest = {
     "./routes/[name].tsx": $_name_,
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
-    "./routes/api/joke.ts": $api_joke,
-    "./routes/greet/[name].tsx": $greet_name_,
     "./routes/hakkimda.tsx": $hakkimda,
     "./routes/index.tsx": $index,
     "./routes/sozler.tsx": $sozler,
