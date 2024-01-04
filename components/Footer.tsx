27f6176ae30b53eb4f789ceb4164f888ddfaf59c
kemalgencay <kemalgencay@gmail.com>
2024-01-04 13:16:22 +0300

    commit 01

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+export function Footer() {
+  return (
+    <footer>
+      <hr class="my-2" />
+      <p>Yakında...</p>
+    </footer>
+  );
+}
+import IconChevronLeft from "https://deno.land/x/tabler_icons_tsx@0.0.5/tsx/chevron-left.tsx"
+import IconChevronRight from "https://deno.land/x/tabler_icons_tsx@0.0.5/tsx/chevron-right.tsx"
