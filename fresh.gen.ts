16b7adc22ca4006cd9282a8f3face82be00ce58b
kemalgencay <kemalgencay@gmail.com>
2024-01-04 21:32:49 +0300

    commit 03

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,25 +2,29 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
+import * as $_name_ from "./routes/[name].tsx";
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $api_joke from "./routes/api/joke.ts";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
+import * as $hakkimda from "./routes/hakkimda.tsx";
 import * as $index from "./routes/index.tsx";
-import * as $Counter from "./islands/Counter.tsx";
+import * as $sozler from "./routes/sozler.tsx";
+
 import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
+    "./routes/[name].tsx": $_name_,
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
     "./routes/api/joke.ts": $api_joke,
     "./routes/greet/[name].tsx": $greet_name_,
+    "./routes/hakkimda.tsx": $hakkimda,
     "./routes/index.tsx": $index,
+    "./routes/sozler.tsx": $sozler,
   },
-  islands: {
-    "./islands/Counter.tsx": $Counter,
-  },
+  islands: {},
   baseUrl: import.meta.url,
 } satisfies Manifest;
 
